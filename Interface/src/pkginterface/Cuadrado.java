75d50361907ebbbdc22f5aa7692e79134359d0e3
Bryan-Farinango <38628690+Bryan-Farinango@users.noreply.github.com>
2018-07-24 09:26:27 -0500

    Add files via upload

---
+++ Interface/src/pkginterface/Cuadrado.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkginterface;
+
+/**
+ *
+ * @author ESFOT
+ */
+public class Cuadrado implements Figura{
+    private double lado;
+    
+    
+    
+     @Override
+    public double calcularArea(){
+        
+        return 0.0;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+    
+    
+    
+}
